aed9e4a144e30028958f621e474e3ab788a7c334
Jonas Riisnaes <karljonasriisnaes@gmail.com>
2018-06-20 21:03:41 +0100

    Removed

---
+++ nodemcu/defaultsetup.lua
@@ -0,0 +1,10 @@
+--Default user input file
+--save a copy and rename this file to userinput.lua
+--if default setup is selected
+
+SENS = {"soilmoisture"}
+PERIOD = 60
+SPECIES = "palm"
+TYPE = "plant_reading"
+HOST = 'jonriisnae'
+PORT = 1234
