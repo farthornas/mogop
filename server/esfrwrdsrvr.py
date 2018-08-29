36bf3f99a63d1bf6e4c9f856bfaf78c596cbb3b2
Jonas Riisnaes <karljonasriisnaes@gmail.com>
2018-08-29 13:55:30 +0100

    Sorted query and tidyed up ideal and measured comparison

--- server/esfrwrdsrvr.py
+++ server/esfrwrdsrvr.py
@@ -4,7 +4,7 @@ from twisted.internet.protocol import Protocol
 from twisted.internet.protocol import Factory
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ServerEndpoint
-from twisted.internet.defer import inlineCallbacks, returnValue, ensureDeferred
+from twisted.internet.defer import inlineCallbacks, returnValue, ensureDeferred, setDebugging
 from twisted.logger import Logger
 from txes2 import ElasticSearch
 from txes2.exceptions import NoServerAvailable
@@ -12,6 +12,7 @@ from ast import literal_eval
 import json
 import time
 import datetime
+setDebugging(True)
 
 log = Logger()
 
@@ -20,17 +21,19 @@ PLANT_READING = 'plant_reading'
 DATA_TYPE = 'type'
 SPECIES = 'species'
 DEVIATION_FROM = 'deviation_{}'
+SENS_DATA = 's_'
 MIN = '_min'
 MAX = '_max'
-TIMESTAMP = 'timestamp'
+TIMESTAMP = 'tstamp'
 SENDERIP = 'sender_ip'
+INDEX_IDEALS = 'idealstest'
+
+#Ideals query should only return latest entry for the queried species
 
 class Forward2Es(Protocol):
 
     def __init__(self):
         self.elastic = Elastic()
-        #servers = self.elastic.servers
-        #if self.elastic.servers() == []:
 
     def connectionMade(self):
         self.transport.write('{"message":"Welcome to Mogop Server","status":1}')
@@ -40,66 +43,79 @@ class Forward2Es(Protocol):
         print("Data received")
         received_data = literal_eval(received_data)
         print("Data type is:{}".format(received_data['type']))
-        t = self.elastic.esTimestamp()
+        t = timestamp()
         if 'hostname' not in received_data:
-            received_data = self.elastic.add_key_val(received_data, SENDERIP, self.transport.getPeer().host)
+            received_data = add_key_val(received_data, SENDERIP, self.transport.getPeer().host)
         if PLANT_READING in received_data['type']:
-            received_data = self.elastic.add_key_val(received_data, TIMESTAMP, t)
+            sens_data = sens_read_list(received_data)
             ideals_query = self.elastic.query_plant_ideals(received_data)
-            ideals = ideals_query.addCallback(self.filter_ideals)
-            ideals.addErrback(lambda err: err.printTraceback())
-            diff = ideals.addCallback(self.compute_diff, received_data)
-            updated_reading = diff.addCallback(self.elastic.add_dict, received_data)
+            ideals = ideals_query.addCallback(ideals_list)
+            diff = ideals.addCallback(compute_diff, sens_data)
+            updated_reading = diff.addCallback(add_dict, received_data)
             updated_reading.addCallback(self.elastic.index_data)
-            diff.addErrback(lambda err: err.printTraceback())
         if PLANT_IDEALS in received_data['type']:
             print("Ideals about to be indexed")
-            received_data = self.elastic.add_key_val(received_data, 'tstamp', t)
-            p = self.elastic.index_data(received_data, index='idealstest')
+            received_data = self.elastic.add_key_val(received_data, TIMESTAMPIDEALS, t)
+            p = self.elastic.index_data(received_data, index=INDEX_IDEALS)
             p.addErrback(lambda err: err.printTraceback())
 
         self.transport.write("Received data\n")
 
-    def filter_ideals(self, i):
-        """Return only ideals  from query result if there is any"""
-        if i['hits']['total'] > 0:
-            print("Plant ideals found")
-            hits =  i['hits']['hits'][0]['_source']#ideals assuming only one entry
-            print(hits)
-            return hits
-        else:
-            return None
+def sens_read_list(raw_data):
+    """Return list of sensor values from incomming data"""
+    sens_data = {}
+    for data, value in raw_data.items():
+        if SENS_DATA in data:
+            sens_data.update({data:value})
+    return sens_data
 
-    def compute_diff(self, ideals, sens_read):
-        """Method used to calculate difference between ideals and sensor
-        values.
-        Returns a dictionary with difference from ideal where ideals and
-        reading found"""
-        diff = {}
-        print("Computing diff")
-        print("Ideals: {}".format(ideals))
-        print("Readings: {}".format(sens_read))
-        for reading, value in sens_read.items():
-            for ideal, estimate in ideals.items():
-                if reading.strip() != (SPECIES or DATA_TYPE) and ideal.strip() != (SPECIES or DATA_TYPE):
-                    if ideal == reading: #Ideal without max and min value
-                        deviation = (-1)*(int(estimate) - int(value))
+def ideals_list(ideals_entry):
+    """return list of min_ideal/max_ideal for a species"""
+    ideals = {}
+    source = filter_source(ideals_entry)
+    for ideal, estimate in source.items():
+        if MIN in ideal or MAX in ideal:
+            ideals.update({ideal:estimate})
+    return ideals
+
+def filter_source(i):
+    """Return only ideals  from query result if there is any"""
+    if i['hits']['total'] > 0:
+        print("Plant ideals found")
+        hits =  i['hits']['hits'][0]['_source']#ideals assuming only one entry
+        return hits
+    else:
+        return None
+
+def compute_diff(ideals, sens_read):
+    """Method used to calculate difference between ideals and sensor
+    values.
+    Returns a dictionary with difference from ideal where ideals and
+    reading found"""
+    diff = {}
+    print("Ideals: {}".format(ideals))
+    print("Readings: {}".format(sens_read))
+    for reading, value in sens_read.items():
+        for ideal, estimate in ideals.items():
+            if reading.strip() != (SPECIES or DATA_TYPE) and ideal.strip() != (SPECIES or DATA_TYPE):
+                if ideal == reading: #Ideal without max and min value
+                    deviation = (-1)*(int(estimate) - int(value))
+                    d = {DEVIATION_FROM.format(ideal):deviation}
+                    diff.update(d)
+                if MIN in ideal: #Ideal with min value
+                    lim_min_ideal = ideal.split(MIN)[0]
+                    if lim_min_ideal in reading:
+                        deviation = int(value) - int(estimate)
                         d = {DEVIATION_FROM.format(ideal):deviation}
                         diff.update(d)
-                    if MIN in ideal: #Ideal with min value
-                        lim_min_ideal = ideal.split(MIN)[0]
-                        if lim_min_ideal in reading:
-                            deviation = int(value) - int(estimate)
-                            d = {DEVIATION_FROM.format(ideal):deviation}
-                            diff.update(d)
-                    if MAX in ideal: #Ideal with max value
-                        lim_max_ideal = ideal.split(MAX)[0]
-                        if lim_max_ideal == reading:
-                            deviation = int(value) - int(estimate)
-                            d = {DEVIATION_FROM.format(ideal):deviation}
-                            diff.update(d)
-        print('Returned Diff:{}'.format(diff))
-        return diff
+                if MAX in ideal: #Ideal with max value
+                    lim_max_ideal = ideal.split(MAX)[0]
+                    if lim_max_ideal == reading:
+                        deviation = int(value) - int(estimate)
+                        d = {DEVIATION_FROM.format(ideal):deviation}
+                        diff.update(d)
+    print('Returned Diff:{}'.format(diff))
+    return diff
 
 
 class Elastic(object):
@@ -111,40 +127,46 @@ class Elastic(object):
         self.doc_type = doc_type
         self.es = ElasticSearch('{}:{}'.format(host,port) )
 
-    def esTimestamp(self):
-        timestamp = int (time.time()*1000)
-        return timestamp
-
     @inlineCallbacks
     def index_data(self, data, index=None):
         print("Indexing")
-        print(data)
+        tstamp = timestamp()
+        data = add_key_val(data, "tstamp", tstamp)
         if index is None:
             index = self.index
-        i = self.es.index(data, doc_type=self.doc_type, index=index)
+        yield self.es.index(data, doc_type=self.doc_type, index=index)
         print("Data indexed")
-        yield i
 
     @inlineCallbacks
     def query_plant_ideals(self, data):
         species = data.get('species')
-        print("\nQuery for plant: {}".format(species))
-        query = {'query': {'match': {'species': { 'query': species, 'type': 'phrase'}}}}
-        returned = self.es.search(query, doc_type=PLANT_IDEALS, index='test3')
-        yield returned
 
-    def print_results_received(results):
-        print('Result Received: {}'.format(results))
-    
-    def add_key_val(self, data, key, val):
-        data[key] = val
-        return data
+        query = {"sort":[{TIMESTAMP: {"order" : "desc"}}],
+                 "size":1,
+                 "query": {"match_phrase": {"species": {"query":species}}}}
 
-    def add_dict(self, data_update, data):
-        updated = {}
-        updated.update(data_update)
-        updated.update(data)
-        return updated
+        answer = yield self.es.search(query, indexes=INDEX_IDEALS)
+        returnValue(answer)
+
+
+def timestamp():
+    timestamp = int (time.time()*1000)
+    return timestamp
+
+def print_results_received(results):
+    print("Results Received START:")
+    pprint(results)
+    print("Results Received END")
+
+def add_key_val(data, key, val):
+    data[key] = val
+    return data
+
+def add_dict(data_update, data):
+    updated = {}
+    updated.update(data_update)
+    updated.update(data)
+    return updated
 
 
 class Forward2EsFactory(Factory):
